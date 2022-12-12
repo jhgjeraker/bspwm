770c6d4091bdb43a412f7b7ae76e9197775b1238
Johannes H. Gjeraker <johannes.holm.gjeraker@disruptive-technologies.com>
2022-12-12 10:51:05 +0100

    update to support system76-power

--- scripts/compositor-init.sh
+++ scripts/compositor-init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+optimus_nvidia="$(optimus-manager --status | grep 'Current GPU mode : nvidia')"
+system76_nvidia="$(system76-power graphics | grep 'nvidia')"
+
 # We need a compositor to avoid tearing, but picom and
 # nvidia does not work well together, so we choose either.
-if optimus-manager --status | grep 'Current GPU mode : nvidia'; then
+if [ "$optimus_nvidia" ] || [ "$system76_nvidia" ]; then
     # Fix stutter caused by nvidia powermizer switching modes.
     # Lock it at highest mode.
     # Fix screen tearing by enabling composition pipeline.
@@ -10,7 +13,8 @@ if optimus-manager --status | grep 'Current GPU mode : nvidia'; then
     nvidia-settings -a "[gpu:0]/GpuPowerMizerMode=1"
 
     # Only set CompositionPipeline if not already active.
-    if ! nvidia-settings -q CurrentMetaMode -t | grep 'ForceCompositionPipeline=On'; then
+    comp_pipeline="$(nvidia-settings -q CurrentMetaMode -t | grep 'ForceCompositionPipeline=On')"
+    if [[ ! $comp_pipeline ]] && [[ ! $system76_nvidia ]]; then
         nvidia-settings -a CurrentMetaMode="nvidia-auto-select +0+0 { ForceCompositionPipeline = On }"
     fi
 
